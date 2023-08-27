6fedd2233e4f911aba4b8167333195877bf43692
yfz-96 <qdu_zyf@163.com>
2023-08-27 21:32:33 +0800

    Update data, scripts and results

--- scripts/fungi_bacteria_ratio.R
+++ scripts/fungi_bacteria_ratio.R
@@ -8,14 +8,14 @@ colnames(melt_abd)[1] <- "Kingdom"
 melt_abd$group <- meta[match(melt_abd$variable, rownames(meta)), "Group"]
 result <- aggregate(value ~ Kingdom + variable, melt_abd, sum)
 dcast_result <- dcast(result, variable ~ Kingdom)
-dcast_result$ratio <- dcast_result$human / dcast_result$Bacteria
+dcast_result$ratio <- dcast_result$Fungi / dcast_result$Bacteria
+
 dcast_result$group <- meta[match(dcast_result$variable, rownames(meta)), "Group"]
 
-human_bacteria_ratio <- dcast_result[, c("Bacteria", "human", "ratio")]
-rownames(human_bacteria_ratio) <- dcast_result$variable
-human_bacteria_ratio <- t(human_bacteria_ratio)
-write.table(human_bacteria_ratio, "../results/human_bacteria_ratio.txt", sep = "\t", quote = F, row.names = T, col.names = NA)
-
+fungi_bacteria_ratio <- dcast_result[, c("Bacteria", "Fungi", "ratio")]
+rownames(fungi_bacteria_ratio) <- dcast_result$variable
+fungi_bacteria_ratio <- t(fungi_bacteria_ratio)
+write.table(fungi_bacteria_ratio, "../results/fungi_bacteria_ratio.txt", sep = "\t", quote = F, row.names = T, col.names = NA)
 
 dcast_result2 <- dcast_result[, c("variable", "group", "ratio")]
 
@@ -28,8 +28,8 @@ shapiro.test(y)
 wilcoxon_result <- wilcox.test(x, y)
 wilcoxon_result
 
-x <- log10(x+1e-60)
-y <- log10(y+1e-60)
+x <- log10(x + 1e-60)
+y <- log10(y + 1e-60)
 
 max_length <- max(length(x), length(y))
 
@@ -37,20 +37,14 @@ max_length <- max(length(x), length(y))
 x <- c(x, rep(median(x), max_length - length(x)))
 y <- c(y, rep(median(y), max_length - length(y)))
 
-data <- data.frame(Clean_Denture = x, Unclean_Denture = y, stringsAsFactors = FALSE)
-colnames(data) <- c("Clean prosthesis", "Unclean prosthesis")
+data <- data.frame(Clean_Prosthesis = x, Unclean_Prosthesis = y, stringsAsFactors = FALSE)
 df <- melt(data)
-
 # 绘制盒图
 p <- ggplot(df, aes(x = variable, y = value, fill = variable)) + 
   geom_boxplot(fill = c("#3F5F9E", "#AC363E"), color = "black", outlier.shape = NA) +
-  labs(x = "Group", y = "log10 (Ratio of human and bacteria)") +
-  geom_text(x = 1.5, y = 0, label = paste("p =", round(wilcoxon_result$p.value, 3))) +
-  coord_cartesian(ylim = c(-40, max(x, y))) +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank())
+  labs(x = "Group", y = "Ratio") +
+  geom_text(x = 1.5, y = 1, label = paste("p =", round(wilcoxon_result$p.value, 3))) +
+  theme_bw()
 
-ggsave("../results/human_bacteria_ratio_boxplot.pdf", p, width = 4, height = 5)
+ggsave("../results/fungi_bacteria_ratio_boxplot.pdf", p, width = 4, height = 5)
 
